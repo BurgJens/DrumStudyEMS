af37bbb32a98635e92d466c8f83de8ebb36fde93
BurgJens <91873641+BurgJens@users.noreply.github.com>
2023-09-18 11:22:30 +0200

    window start pos

--- Desktop/src/main/kotlin/Main.kt
+++ Desktop/src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPosition
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import view.AppNavigation
@@ -36,7 +38,7 @@ fun main() = application {
 
     Window(
         onCloseRequest = ::exitApplication,
-        state = WindowState(size = appWindowSize)
+        state = WindowState(size = appWindowSize, position = WindowPosition(Alignment.TopCenter))
     ) {
         App()
     }
